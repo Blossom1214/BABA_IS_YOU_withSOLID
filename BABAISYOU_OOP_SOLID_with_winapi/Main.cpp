7414b12471f9519aa87cb990ce0a8ddfe1d249b8
namseungwoo <52452543+Blossom1214@users.noreply.github.com>
2025-04-30 04:14:07 +0900

    또 오류난다!

--- BABAISYOU_OOP_SOLID_with_winapi/Main.cpp
+++ BABAISYOU_OOP_SOLID_with_winapi/Main.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 #include"Windows.h"
 using namespace std;
-
+void MoveCursorToTop() //화면 깜밖임을 줄이기위해 커서 를 처음으로 셋팅해서 다시 그리는 용도로사용함
+{
+    COORD coord = { 0, 0 };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+void HideCursor() //커서를 하이딩 해서 핸들(콘솔윈도우창)에서 지움
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursorInfo;
+    cursorInfo.dwSize = 1;
+    cursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(hConsole, &cursorInfo);
+}
 int main()
 {
 	return 0;
