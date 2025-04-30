c6bfdabd1a31adb059e61c8594c5bdf9753c7782
namseungwoo <52452543+Blossom1214@users.noreply.github.com>
2025-04-30 21:45:35 +0900

    일단 기본 슬라이드구조완성
    구조완성은 했으나 주석은 내일 달기...

--- BABAISYOU_OOP_SOLID_with_winapi/RuleManager.cpp
+++ BABAISYOU_OOP_SOLID_with_winapi/RuleManager.cpp
@@ -3,7 +3,9 @@
 #include "TileObjectBase.h"
 #include "GrammerManager.h"
 #include <algorithm>
-
+#include "Tile.h"
+#include"Movement.h"
+#include "TextTile.h"
 RuleManager::RuleManager(TileMap* map):_map(map)
 {
 }
@@ -19,13 +21,41 @@ void RuleManager::OnNotify(TileObjectBase* changedObj)
 std::vector<TextTile*> RuleManager::SlideChainFrom(const Position& start, Direction dir, int maxDepth)
 {
 	std::vector<TextTile*> chaine;
+	Position currentPos = start;
+	Position delta = Movement::Vector(dir);
+	for (int i = 0; i < maxDepth; ++i)
+	{
+		if (!_map->Isinside(currentPos))
+			break;
+		Tile* tile = _map->GetTile(currentPos);
+		if (!tile)break;
+		bool foundTextTile = false;
 
-
-
-
-	return std::vector<TextTile*>();
+		for (TileObjectBase* obj : tile->GetObjects()) 
+		{
+			if (TextTile* Text = dynamic_cast<TextTile*>(obj))
+			{
+				chaine.push_back(Text);
+				foundTextTile = true;
+				break;
+			}
+		}
+		
+		if (!foundTextTile) break;
+		currentPos = currentPos + delta;
+	}
+	return chaine;
 }
-
+//1. 체인 벡터 선언
+//2. 현재 위치 = start
+//3. maxDepth 만큼 반복 :
+//├─ 맵 범위 확인
+//├─ 타일 가져오기
+//├─ TextTile * 있는지 확인(dynamic_cast)
+//└ 없으면 break
+//├─ 있으면 체인에 추가
+//└ 다음 위치로 이동(Position + DirectionVector)
+//4. 반환: 체인
 bool RuleManager::HasRule(ObjectType obj, RuleType rule) const
 {
 	
