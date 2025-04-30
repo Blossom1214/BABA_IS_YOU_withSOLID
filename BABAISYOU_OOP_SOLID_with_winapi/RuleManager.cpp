c04550e59c70c3be8deb7c12fb3a174966155d1c
namseungwoo <52452543+Blossom1214@users.noreply.github.com>
2025-04-30 16:55:56 +0900

    룰제작중...
    제작중....야랄...

--- BABAISYOU_OOP_SOLID_with_winapi/RuleManager.cpp
+++ BABAISYOU_OOP_SOLID_with_winapi/RuleManager.cpp
@@ -1,1 +1,57 @@
 #include "RuleManager.h"
+#include "TileMap.h"
+#include "TileObjectBase.h"
+#include "GrammerManager.h"
+#include <algorithm>
+
+RuleManager::RuleManager(TileMap* map):_map(map)
+{
+}
+
+void RuleManager::InitialParse()
+{
+}
+
+void RuleManager::OnNotify(TileObjectBase* changedObj)
+{
+}
+
+bool RuleManager::HasRule(ObjectType obj, RuleType rule) const
+{
+	
+	auto it = _rules.find(obj);
+	if (it == _rules.end())
+	{
+		return false;
+	}
+	return it->second.find(rule) != it->second.end();
+}
+
+const std::unordered_set<RuleType>& RuleManager::GetRules(ObjectType obj) const
+{
+	static const std::unordered_set<RuleType> empty;
+	auto it = _rules.find(obj);
+	if (it == _rules.end())
+	{
+		return empty;
+	}
+	return it->second;
+}
+
+void RuleManager::RegisterParseTarget(const Position& pos)
+{
+	_parseTargets.push_back(pos);
+}
+
+void RuleManager::AddRule(ObjectType subject, RuleType rule)
+{
+	_rules[subject].insert(rule);
+}
+
+void RuleManager::RemoveRulesLinkedTo(Position center)
+{
+}
+
+void RuleManager::UpdateRulesAt(Position center)
+{
+}
--- BABAISYOU_OOP_SOLID_with_winapi/RuleManager.h
+++ BABAISYOU_OOP_SOLID_with_winapi/RuleManager.h
@@ -5,11 +5,15 @@
 #include "RuleType.h"
 #include "ObjectType.h"
 #include "Position.h"
+#include "MYCustom.h"
 class TileMap;
-class GrammerManager;
 class TileObjectBase;
+using namespace MYCustom::Uitility::Hash;
+
+
 class RuleManager
 {
+	friend class GrammerManager;
 public:
 	RuleManager(TileMap* map);//생성자
 	RuleManager(const RuleManager&) = delete; //복사생성은 하지않음!
@@ -18,19 +22,20 @@ public:
 	void InitialParse();
 	void OnNotify(TileObjectBase* changedObj);//알림 수신용 함수(실시간 상태확인용)
 
-	bool HasRule(ObjectType obj, RuleType rule) const;
-	const std::unordered_set<RuleType>& GetRules(ObjectType obj) const;
-
+	bool HasRule(ObjectType obj, RuleType rule) const; //이오브젝트에 이 룰이 있나요?!
+	const std::unordered_set<RuleType>& GetRules(ObjectType obj) const;//해당오브젝트에 있는 룰테이블읽기!
+	void RegisterParseTarget(const Position& pos);//텍스트타일이며.. 명사의 위치!(저장용..)(맵생성시 최초만)
 private:
 	
-	void AddRule(ObjectType subject, RuleType rule);
+	void AddRule(ObjectType subject, RuleType rule); //문장해석후 룰추가
 	void RemoveRulesLinkedTo(Position center);
 
-	
+	//방금 실제 게임해보면서 깨달은 또다른 규칙들 그냥 내가 너무 어렵게 생각햇음
 	void UpdateRulesAt(Position center);
 private:
 	TileMap* _map;
 	GrammerManager* _grammerManager;
+	std::vector<Position> _parseTargets;
 	std::unordered_map<ObjectType, std::unordered_set<RuleType>> _rules;
 
 };
